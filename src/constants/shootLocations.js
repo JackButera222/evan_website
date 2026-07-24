63fe451104a3f954ce0b01c9a3bbe9748a261b57
Jack Butera <jbutera@udel.edu>
2026-07-24 11:09:25 -0400

    bug fixes

--- src/constants/shootLocations.js
+++ src/constants/shootLocations.js
@@ -5,10 +5,10 @@
 // `note` is optional extra text in the popup.
 export const SHOOT_LOCATIONS = [
 { label: "Kenneth Hahn State Recreation Area", address: "4100 S La Cienega Blvd, Los Angeles, CA 90056" },
-{ label: "Venice Beach Pier", address: "3302 Ocean Front Walk, Marina Del Rey, CA 90292" },
+{ label: "Venice Beach Pier", address: "10 Washington Blvd, Venice, CA 90292" },
 { label: "Ernest E. Debs Regional Park", address: "3606 Boundary Ave, Los Angeles, CA 90031" },
 { label: "Los Angeles Historic Park", address: "1245 N Spring St, Los Angeles, CA 90012" },
 { label: "Griffith Park", address: "4730 Crystal Springs Dr, Los Angeles, CA 90027" },
 { label: "Whitnall Highway Park South", address: "600 N Whitnall Hwy, Burbank, CA 91505" },
-{ label: "Lake Balboa Trail", address: "6300 Balboa Blvd, Van Nuys, CA 91406" },
+{ label: "Lake Balboa Trail", address: "6331 Woodley Ave, Van Nuys, CA 91406" },
 ];
